c1fbae68eb1413b4369ce038472d68d4122c46fa
Darwin Gualotuña <daosgc@gmail.com>
2021-01-14 08:47:44 -0500

    Clear and get all values from the form

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.Windows;
+﻿using System;
+using System.Windows;
 
 namespace Practica1
 {
@@ -14,8 +15,44 @@ namespace Practica1
 
         private void btnAceptar_Click(object sender, RoutedEventArgs e)
         {
-            Window1 window1 = new Window1();
-            window1.Show();
+            // Obtener valores del formulario
+            string productName = txtBxName.Text;
+            string productSize = txtBxSize.Text;
+            string productType = cmbBoxType.SelectedIndex >= 0 ? cmbBoxType.Text : "";
+            string firstAddress = chckBoxPrincipal.IsChecked == true ? "Calle de la Rosa n. 28 " : "";
+            string secondAddress = chckBoxSecondary.IsChecked == true ? "Calle Alcazabilla n. 3" : "";
+            string distributor = "";
+            if (rdBtn1.IsChecked == true)
+            {
+                distributor = rdBtn1.Content.ToString();
+            }
+            else if (rdBtn2.IsChecked == true)
+            {
+                distributor = rdBtn2.Content.ToString();
+            }
+            else if (rdBtn3.IsChecked == true)
+            {
+                distributor = rdBtn3.Content.ToString();
+            }
+            Console.WriteLine("productName->"+productName);
+            Console.WriteLine("productSize->" + productSize);
+            Console.WriteLine("productType->" + productType);
+            Console.WriteLine("address->" + firstAddress + secondAddress);
+            Console.WriteLine("distributor->" + distributor);
+            //Window1 window1 = new Window1();
+            //window1.Show();
+        }
+
+        private void btnClear_Click(object sender, RoutedEventArgs e)
+        {
+            txtBxName.Clear();
+            txtBxSize.Clear();
+            cmbBoxType.SelectedIndex = -1;
+            chckBoxPrincipal.IsChecked = false;
+            chckBoxSecondary.IsChecked = false;
+            rdBtn1.IsChecked = false;
+            rdBtn2.IsChecked = false;
+            rdBtn3.IsChecked = false;
         }
     }
 }
