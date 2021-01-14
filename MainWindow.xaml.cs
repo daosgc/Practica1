83dc7eaec4c1fbe1586c42ef07b351238c1f0f42
Darwin Gualotuña <daosgc@gmail.com>
2021-01-14 07:58:09 -0500

    Remove unnecessary code

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,17 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+﻿using System.Windows;
 
 namespace Practica1
 {
@@ -23,11 +10,6 @@ namespace Practica1
         public MainWindow()
         {
             InitializeComponent();
-        }
-
-        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
-        {
-
         }
 
         private void btnAceptar_Click(object sender, RoutedEventArgs e)
